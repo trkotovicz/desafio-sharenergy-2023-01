0dc4f72e507b2791a185fb7d2b140698f4538413
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-01-16 18:52:33 -0300

    build: [frontend] instala o Typescript com React

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>.App</p>
+        <h3>SHARENERGY</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/scripts/kill_all.sh
@@ -0,0 +1,2 @@
+kill -9 $(lsof -w -n -i tcp:3000)
+kill -9 $(lsof -w -n -i tcp:3001)
