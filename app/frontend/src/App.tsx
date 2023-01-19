a2b31c1e7928eacb566d642d8ac2d6d7df093079
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-01-19 20:59:11 -0300

    feat: [frontend] Cria a página http status cats

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Clients from './pages/Clients';
+import HttpCats from './pages/HttpCats';
 import Login from './pages/Login';
 import RandomUsers from './pages/RandomUsers';
+import './App.css';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={ <Login /> } />
         <Route path="/clients-api" element={ <Clients /> } />
         <Route path="/random-users" element={ <RandomUsers /> } />
+        <Route path="/http-cats" element={ <HttpCats /> } />
       </Routes>
     </BrowserRouter>
   );
---
+++ app/frontend/src/pages/HttpCats.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function HttpCats() {
+  const [status, setStatus] = useState('418');
+
+  const url = `https://http.cat/${status}`
+
+  return (
+    <main>
+      <h3>Http Cats</h3>
+
+      <input
+        className='http-status-input'
+        type='number'
+        maxLength={3}
+        placeholder='http status code'
+        value={ status }
+        onChange={ ({target}) => { setStatus(target.value) } }  
+      />
+      <img className='cat-img' src={ url } alt={ `http status code ${status}`} />
+    </main>
+  )
+}
